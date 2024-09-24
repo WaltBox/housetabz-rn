5967bd126007f37ba4b5595ece0c1806fbe286f2
Nihal Neerukonda <nihal.neerukonda@utexas.edu>
2024-09-24 17:17:38 -0500

    temp background for company cover photo + restyled info boxes

--- src/screens/ViewCompanyCard.js
+++ src/screens/ViewCompanyCard.js
@@ -7,11 +7,12 @@ const ViewCompanyCard = ({ route }) => {
   return (
     <ScrollView style={styles.container}>
       {/* Cover Image */}
-  
+      <View style={styles.coverImageContainer}>
+        <Image style={styles.coverImage} />
+      </View>
 
       {/* Company Details Container */}
       <View style={styles.companyDetailsContainer}>
-    
         <Text style={styles.title}>{title}</Text>
         <Text style={styles.avgCost}>Avg Cost / Roommate: {price}</Text>
         <Button title="View Plans" onPress={() => { /* Action for viewing plans */ }} />
@@ -26,7 +27,7 @@ const ViewCompanyCard = ({ route }) => {
       <View style={styles.textSection}>
         <Text style={styles.sectionTitle}>About</Text>
         <Text style={styles.paragraph}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. 
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum.
         </Text>
         <Text style={styles.paragraph}>
           Cras vehicula, dolor at placerat elementum, risus felis viverra urna, vel consequat velit sapien id metus.
@@ -50,33 +51,33 @@ const ViewCompanyCard = ({ route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
     backgroundColor: '#f8f8f8',
+  },
+  coverImageContainer: {
+    backgroundColor: '#FFA500', // putting orange bg as placeholder for now
+    height: 150, 
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   coverImage: {
     width: '100%',
-    height: '12.5%',  // 1/8th of the screen height
-    borderRadius: 10,
-    marginBottom: 20,
+    height: '100%',
+    resizeMode: 'cover', 
   },
   companyDetailsContainer: {
-    width: '100%',
+    marginTop: -50,
     padding: 20,
-    backgroundColor: '#fff',
+    backgroundColor: '#d3d3d3', 
     borderRadius: 10,
-    marginBottom: 20,
+    marginHorizontal: 20,
+    alignItems: 'center',
+    borderColor: '#000', 
+    borderWidth: 1,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
     shadowRadius: 4,
     elevation: 3,
-    alignItems: 'center',
-  },
-  companyLogo: {
-    width: 100,
-    height: 100,
-    borderRadius: 10,
-    marginBottom: 10,
   },
   title: {
     fontSize: 24,
@@ -90,25 +91,28 @@ const styles = StyleSheet.create({
     color: '#555',
   },
   specialDealsContainer: {
-    width: '100%',
+    marginTop: 20,
     padding: 20,
-    backgroundColor: 'grey',  // Similar style to the Special Deals
+    backgroundColor: '#d3d3d3',
     borderRadius: 10,
-    marginBottom: 20,
+    marginHorizontal: 20,
+    alignItems: 'center',
+    borderColor: '#000', 
+    borderWidth: 1,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
     shadowRadius: 4,
     elevation: 3,
-    alignItems: 'center',
   },
   specialDealsText: {
     fontSize: 18,
     fontWeight: 'bold',
-    color: '#fff',
+    color: '#555',
   },
   textSection: {
-    marginBottom: 20,
+    marginHorizontal: 20,
+    marginTop: 20,
   },
   sectionTitle: {
     fontSize: 20,
