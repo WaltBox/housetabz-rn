e7fac6a1d194ae333333a36b9722a713a7dc18b3
ronakjain1234 <ronakjain0919@utexas.edu>
2024-10-03 18:23:41 -0500

    Inital code for MyHousePage

--- src/screens/MyHouseScreen.js
+++ src/screens/MyHouseScreen.js
@@ -1,22 +1,51 @@
 import React from 'react';
-import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
-const MyHouseScreen = () => {
-    return (
-        <View style={styles.container}>
-            <TouchableOpacity>
-                <Text>
-                    Join A House
-                </Text>
-            </TouchableOpacity>
-        </View>
-    );
-};
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F8F8F8',
-    },
-});
-export default MyHouseScreen;
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import './HouseTabz.css';
+
+const HouseTabz = () => {
+  return (
+    <div className="container">
+      <header className="header">
+        <h1 className="app-name">HouseTabz</h1>
+        <div className="header-icons">
+          <span role="img" aria-label="notification" className="icon">🔔</span>
+          <span role="img" aria-label="profile" className="icon">👤</span>
+        </div>
+      </header>
+
+      <div className="house-status-container">
+        <div className="progress-bar-container">
+          <CircularProgressbar 
+            value={70} 
+            text={`HSI`} 
+            styles={buildStyles({
+              textSize: '16px',
+              pathColor: '#77dd77',
+              textColor: '#77dd77',
+              trailColor: '#f5f5f5',
+            })}
+          />
+        </div>
+        <div className="house-status">House Status: Great</div>
+      </div>
+
+      <div className="score-board-container">
+        <h2 className="section-title">Score Board</h2>
+        <div className="score-board"></div>
+      </div>
+
+      <div className="current-tab-container">
+        <h2 className="section-title">CurrentTab</h2>
+        <div className="tab"></div>
+      </div>
+
+      <div className="paid-tabz-container">
+        <h2 className="section-title">PaidTabz</h2>
+        <div className="tab"></div>
+      </div>
+    </div>
+  );
+}
+
+export default HouseTabz;
